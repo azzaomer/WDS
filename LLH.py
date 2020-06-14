b917cb3235bd49d96e0a0b3fc1b881052fe020a7
azzaomer <48832222+azzaomer@users.noreply.github.com>
2020-06-14 18:26:33 +0200

    Update LLH.py

--- LLH.py
+++ LLH.py
@@ -1,11 +1,11 @@
-	#1- change one pipe
+	##################################################LLHs :##########################################################
+ #1- change one pipe
 def Change(solution, NumberOfPipeSizesAvailable):
     s = random.randint(0, len(solution) - 1)
     solution[s] =  random.randint(0, NumberOfPipeSizesAvailable - 1)
     return solution
 
-    # 2- change 2 pipes randomly :
-
+ # 2- change 2 pipes randomly :
 def ChangeTowPipes(solution, NumberOfPipeSizesAvailable):
     s = random.randint(0, len(solution) - 1)
     solution[s] =  random.randint(0, NumberOfPipeSizesAvailable - 1) 
@@ -13,43 +13,43 @@ def ChangeTowPipes(solution, NumberOfPipeSizesAvailable):
     s = random.randint(0, len(solution) - 1)
     solution[s] =  random.randint(0, NumberOfPipeSizesAvailable - 1)
     return solution
-    # 3- swap operator :
-def swap_random(solution):
+
+# 3- swap operator :
+def Swap_random(solution):
      idx = range(len(solution))
      i1, i2 = random.sample(idx, 2)
      solution[i1], solution[i2] = solution[i2], solution[i1] 
      return solution
      
 
-
 # 4-Increase or Decrease all pipe sizes by one:     
-def IncOrDecAllPipesByOne(solution):
-        k=random.randint(1,2)
+def IncOrDecAllByOne(solution):
+        k = random.randint(1,2)
         for s in range(len(solution)):
-            #Decrease
-            if (solution[s] > 0 and k==1):
+        #Increase   
+            if(solution[s] < (NumberOfPipeSizesAvailable-1) and k==2):
+                solution[s] += 1
+        #Decrease
+            elif(solution[s] > 0 and k==1):
                 solution[s] -= 1
-            #Increase   
-            elif(solution[s] < (NumberOfPipeSizesAvailable-1) and k==2):
-                solution[s] += 1 
+             
                 
         return solution
               
 #5-Increase or decrease a randomly selected pipe diameter by one pipe size:
-def RandomlyDecOrInc(solution,NumberOfPipeSizesAvailable):
+def IncOrDecRandomly(solution,NumberOfPipeSizesAvailable):
     s = random.randint(0, len(solution) - 1)
-    k=random.randint(1,2)
-    #Decrease
-    if (solution[s] > 0 and k==1):
-        solution[s] -= 1
+    k = random.randint(1,2)
     #Increase
-    elif(solution[s] < (NumberOfPipeSizesAvailable-1) and k==2):
-        solution[s] += 1 
-        
+    if(solution[s] < (NumberOfPipeSizesAvailable-1) and k==1):
+        solution[s] += 1
+    #Decrease
+    elif (solution[s] > 0 and k==2):
+        solution[s] -= 1    
     return solution
   
 #6- randomly change from 1 to 5 pipes :
-def changeInRange(solution,NumberOfPipeSizesAvailable):
+def ChangeInRange(solution,NumberOfPipeSizesAvailable):
     k = random.randint(1,5)
     #print(k ," pipes will be changed")
     idx= range(len(solution))
@@ -58,16 +58,11 @@ def changeInRange(solution,NumberOfPipeSizesAvailable):
     for i in range(len(s)):
         solution[s[i]]=random.randint(0,NumberOfPipeSizesAvailable-1)
     return solution
+   
+
+#7-randomly initalize solution and pick 2 random pipes increase one and decrease one
+def IncreaseAndDecreaseP2(NumberOfPipes,NumberOfPipeSizesAvailable):
     
-#7-
-def Randomise(solution, NumberOfPipes, NumberOfPipeSizesAvailable):
-    for s in range(NumberOfPipes):
-            solution[s] = random.randint(0, NumberOfPipeSizesAvailable - 1) 
-    return solution
-#8-randomly initalize solution and pick 2 random pipes increase one and decrease one
-def NewMethod1(NumberOfPipes,NumberOfPipeSizesAvailable):
-    i = random.randint(0,NumberOfPipeSizesAvailable-1)
-    solution = [i] * NumberOfPipes
     s = random.randint(0, len(solution) - 1)
     if solution[s] < (NumberOfPipeSizesAvailable-1):
         solution[s] += 1
@@ -75,9 +70,9 @@ def NewMethod1(NumberOfPipes,NumberOfPipeSizesAvailable):
     if solution[r] > 0 and r!= s:
         solution[r] -= 1
     return solution
-#9-intialize solution to average pipe size and pick 4 rando pipes +2 and -2
-def NewMethod2(NumberOfPipes,NumberOfPipeSizesAvailable):
-    solution = [6] * NumberOfPipes
+
+#8-pick 4 rando pipes +2 and -2
+def IncreasndDecreaseP4(NumberOfPipes,NumberOfPipeSizesAvailable):
     s = random.randint(0, len(solution) - 1)
     if solution[s] < (NumberOfPipeSizesAvailable-1):
         solution[s] += 1
@@ -91,3 +86,26 @@ def NewMethod2(NumberOfPipes,NumberOfPipeSizesAvailable):
     if solution[n] > 0 and n!=s and n!=p and n!=r:
         solution[n] -= 1
     return solution
+#9
+def Randomise(solution, NumberOfPipes, NumberOfPipeSizesAvailable):
+    for s in range(NumberOfPipes):
+            solution[s] = random.randint(0, NumberOfPipeSizesAvailable - 1) 
+    return solution
+    
+#Dictionary mapping for functions 
+def Operaters(LLH):
+  
+    switcher = {
+        1: lambda :Change(solution, NumberOfPipeSizesAvailable),
+        2: lambda :ChangeTowPipes(solution, NumberOfPipeSizesAvailable),
+        3: lambda :Swap_random(solution),
+        4: lambda :IncOrDecAllByOne(solution),
+        5: lambda :IncOrDecRandomly(solution,NumberOfPipeSizesAvailable),
+        6: lambda :ChangeInRange(solution,NumberOfPipeSizesAvailable),
+        7: lambda :IncreaseAndDecreaseP2(NumberOfPipes,NumberOfPipeSizesAvailable),
+        8: lambda :IncreasndDecreaseP4(NumberOfPipes,NumberOfPipeSizesAvailable),
+        9: lambda :Randomise(solution, NumberOfPipes, NumberOfPipeSizesAvailable)
+    }
+    # Get the function from switcher dictionary
+    func = switcher.get(LLH, lambda: "Invalid operator")
+    return func()
